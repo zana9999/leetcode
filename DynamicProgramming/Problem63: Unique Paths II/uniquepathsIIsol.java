59b021a9a5f7667200683d65d18f4505f85be492
zana9999 <211172546+zana9999@users.noreply.github.com>
2025-10-14 18:23:41 -0400

    Create uniquepathsIIsol.java

---
+++ DynamicProgramming/Problem63: Unique Paths II/uniquepathsIIsol.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] grid) {
+
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        if (grid[0][0] == 1 || grid[m-1][n-1] == 1){
+            return 0;
+        }
+
+        int[][] memo = new int [m][n];
+        for(int[] row: memo){
+            Arrays.fill(row, -1);
+        }
+
+        return helper(grid, 0, 0, m , n, memo);
+
+    }
+
+
+    public int helper(int[][] grid, int i, int j, int m, int n, int [][] memo){
+        if(i >= m || j >= n || grid[i][j] == 1){
+            return 0;
+        }
+
+        if(i == m-1 && j == n-1){
+            return 1;
+        }
+
+        if(memo[i][j] != -1){
+            return memo[i][j];
+        }
+
+        memo[i][j]= helper(grid, i + 1, j, m , n, memo ) + helper(grid, i, j + 1, m , n , memo);
+        return memo[i][j];
+    }
+}
