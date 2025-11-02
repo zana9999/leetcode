e47d163d332ba869cd3ee05724c2c4e591b4a3e1
zana9999 <211172546+zana9999@users.noreply.github.com>
2025-11-01 21:16:59 -0400

    Create pascalstriangle.java

---
+++ DynamicProgramming/Problem 118: Pascal's Triangle/pascalstriangle.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        triangle.add(new ArrayList<>(List.of(1)));
+
+        for(int i = 1; i < numRows; i++){
+            List<Integer> prevRow = triangle.get(i-1);
+            List<Integer> newRow = new ArrayList<>();
+
+            newRow.add(1);
+
+            for(int j = 1; j < i; j++){
+                int sum = prevRow.get(j-1) + prevRow.get(j);
+                newRow.add(sum);
+            }
+
+            newRow.add(1);
+            triangle.add(newRow);
+        }
+
+        return triangle;
+
+    }
+}
